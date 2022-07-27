ba00b5585cb90500099e7aa0885be15d9ff9829e
Carlos Zabala <czabala847@gmail.com>
2022-07-27 15:20:57 -0500

    Protegiendo rutas con guards y persistienso sesion en localstorage

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { catchError, map, Observable, of, tap } from 'rxjs';
 import {
@@ -18,34 +18,65 @@ export class AuthService {
   constructor(private http: HttpClient) {}
 
   login(email: string, password: string): Observable<LoginResponse> {
-    return this.http
-      .post<AuthResponse>(`${this.urlBase}/auth`, {
-        email,
-        password,
-      })
-      .pipe(
-        tap((response) => {
+    const url: string = `${this.urlBase}/auth`;
+    const body = {
+      email,
+      password,
+    };
+
+    return this.http.post<AuthResponse>(url, body).pipe(
+      tap((response) => {
+        if (response.ok) {
           this._user = {
             name: response.name!,
             uid: response.uid!,
           };
-        }),
-        map((response) => {
-          return {
-            success: response.ok,
-            msg: '',
-          };
-        }),
-        catchError((err) => {
-          return of({
-            success: false,
-            msg: err.error.msg,
-          });
-        })
-      );
+
+          localStorage.setItem('token', response.token!);
+        }
+      }),
+      map((response) => {
+        return {
+          success: response.ok,
+          msg: '',
+        };
+      }),
+      catchError((err) => {
+        return of({
+          success: false,
+          msg: err.error.msg,
+        });
+      })
+    );
   }
 
   get user() {
     return this._user;
+  }
+
+  validateToken(): Observable<boolean> {
+    const url: string = `${this.urlBase}/auth/renew`;
+    const headers: HttpHeaders = new HttpHeaders().set(
+      'x-token',
+      localStorage.getItem('token') || ''
+    );
+
+    return this.http.get<AuthResponse>(url, { headers }).pipe(
+      map((response) => {
+        if (response.ok) {
+          this._user = {
+            name: response.name!,
+            uid: response.uid!,
+          };
+
+          localStorage.setItem('token', response.token!);
+        }
+
+        return response.ok;
+      }),
+      catchError((err) => {
+        return of(false);
+      })
+    );
   }
 }
---
+++ src/app/guards/validate-token.guard.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, CanLoad, Router } from '@angular/router';
+import { Observable, tap } from 'rxjs';
+
+import { AuthService } from '../auth/services/auth.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ValidateTokenGuard implements CanActivate, CanLoad {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(): Observable<boolean> | boolean {
+    return this.authService.validateToken().pipe(
+      tap((valid) => {
+        if (!valid) {
+          this.router.navigate(['/auth']);
+        }
+      })
+    );
+  }
+  canLoad(): Observable<boolean> | boolean {
+    return this.authService.validateToken().pipe(
+      tap((valid) => {
+        if (!valid) {
+          this.router.navigate(['/auth']);
+        }
+      })
+    );
+  }
+}
